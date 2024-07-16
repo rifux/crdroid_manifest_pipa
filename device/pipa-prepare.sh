107173a43ad9e2b28dfe15776579acd045b0cfa9
Blinkov `rifux` Vladimir ♪ <87264036+rifux@users.noreply.github.com>
2024-07-16 14:59:02 +0300

    pipa: fix "non-existent modules" build error

--- device/pipa-prepare.sh
+++ device/pipa-prepare.sh
@@ -9,7 +9,7 @@ git clone https://github.com/ai94iq/proprietary_vendor_xiaomi_pipa -b lmou ../cr
 
 # Device
 git clone https://github.com/rifux/android_device_xiaomi_pipa -b lineage-21 ../crdroid-lindroid/device/xiaomi/pipa
-git clone https://github.com/pachdomenic/android_device_xiaomi_sm8250-common -b lineage-21 ../crdroid-lindroid/device/xiaomi/sm8250-common
+git clone https://github.com/rifux/android_device_xiaomi_sm8250-common -b lineage-21 ../crdroid-lindroid/device/xiaomi/sm8250-common
 
 # Kernel
 git clone https://github.com/rifux/android_kernel_xiaomi_sm8250 -b lineage-21 ../crdroid-lindroid/kernel/xiaomi/sm8250
